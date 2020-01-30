66d1b9fab36119a561760dfe4b7fdf8fc9fc2230
EmirVelazquez <emirleovelazquez@gmail.com>
2020-01-29 23:18:16 -0600

    navbar changes to html as well as css. added logic for navbar active link status. app.js script link to main handlebars. complete change for navbar, new navbar works much better for mobile

---
+++ public/js/app.js
@@ -0,0 +1,18 @@
+$(document).ready(function () {
+    const navLink = $("#navBar .navbar-nav li");
+    const activeLinks = $("#navBar .navbar-nav");
+
+    //============================NAV LOGIC BELOW=====================================//
+    //This block adds and removes active state on links
+    navLink.on("click", function () {
+        activeLinks.find("a.active").removeClass("active");
+        $(this).children("a").addClass("active");
+    });
+    //This block revomes active state from all links if my name is clicked
+    // $("#newsGramLogo").on("click", function () {
+    //     $("#navBar .navbar-nav").find("li.active").removeClass("active");
+    //     $("#aboutLink").addClass("active");
+    // });
+    //============================NAV LOGIC ABOVE=====================================//
+
+});
